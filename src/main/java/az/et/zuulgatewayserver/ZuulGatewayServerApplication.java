9aba646914b02fcad5e1ea49821fef0b6471f6e0
Elvin Taghizade Taghi <elvintaghiyev184@gmail.com>
2022-02-05 12:12:58 +0400

    initial commit

---
+++ src/main/java/az/et/zuulgatewayserver/ZuulGatewayServerApplication.java
@@ -0,0 +1,22 @@
+package az.et.zuulgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableCircuitBreaker
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+public class ZuulGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayServerApplication.class, args);
+    }
+
+}
